8d84d695d9b439a741d630325b8a3dba0bda1083
Shree Harsha <shreeharshaboyapati042@gmail.com>
2026-02-10 19:31:57 +0530

    fix: added the missing metadata and job controller

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import user from './user.js';
+import job from './job.js';
 const router: express.Router = express.Router();
 
 router.use('/user', user);
+router.use('/job', job);
 
 export default router;
